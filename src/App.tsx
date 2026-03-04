be71ea756c73b27b34301707ea4747755c35c7bc
javierrama1976-creator <javierrama1976@gmail.com>
2026-03-04 18:55:49 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -197,7 +197,8 @@ Cuando cierras la puerta, tu perro no sabe si vas a volver.
             {/* Table Surface Effect */}
             
             
-        
+        <div className="relative z-10 overflow-hidden">
+<div className="relative">
                
                 {/* Fondo emocional */}
 <img
