c5e4465aaa56ee2011dc7cf5af80e9b358f84247
javierrama1976-creator <javierrama1976@gmail.com>
2026-03-04 14:06:59 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -212,7 +212,7 @@ export default function App() {
                
                 {/* Fondo emocional */}
 <img
-  "/dog-door-anxiety.jpg.jpeg"
+src="/dog-anxiety-hero.jpeg"
   alt="Perro con ansiedad por separación"
   className="absolute inset-0 w-full h-full object-cover opacity-80"
 />
